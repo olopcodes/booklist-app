47cbfa4ad78d410b8687776000af406d60dcc7eb
olopcodes <olopcodes@gmail.com>
2021-04-30 22:10:13 -0400

    finished the mobile styling for the app

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import react from "react";
+import reactDOM from "react-dom";
+import "./css/styles.scss";
+import "./js/main.js";
+import image from "./images/book-img.jpg";
---
+++ webpack.config.js
@@ -0,0 +1,106 @@
+const currentTask = process.env.npm_lifecycle_event;
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    filename: "myBundle.[hash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new ImageMinimizerPlugin({
+      minimizerOptions: {
+        // Lossless optimization with custom option
+        // Feel free to experiment with options for better result for you
+        plugins: [
+          ["gifsicle", { interlaced: true }],
+          ["jpegtran", { progressive: true }],
+          ["optipng", { optimizationLevel: 5 }],
+          [
+            "svgo",
+            {
+              plugins: [
+                {
+                  removeViewBox: false,
+                },
+              ],
+            },
+          ],
+        ],
+      },
+    }),
+  ],
+  mode: "development",
+  devtool: "eval-cheap-source-map",
+  devServer: {
+    port: 8080,
+    contentBase: path.resolve(__dirname, "dist"),
+    hot: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      // {
+      //   test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+      //   type: "asset",
+      //   loader: "file-loader",
+      //   options: {
+      //     name: "[name].[ext]",
+      //     outputPath: "images",
+      //   },
+      // },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+              name: "images/[name].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                { useBuiltIns: "usage", corejs: 3, targets: "defaults" },
+              ],
+              "@babel/preset-react",
+            ],
+          },
+        },
+      },
+    ],
+  },
+};
+
+if (currentTask == "build") {
+  config.mode = "production";
+  config.module.rules[0].use[0] = MiniCssExtractPlugin.loader;
+  config.plugins.push(
+    new MiniCssExtractPlugin({ filename: "css/main.[hash].css" }),
+    new CleanWebpackPlugin(),
+    new WebpackManifestPlugin()
+  );
+}
+
+module.exports = config;
