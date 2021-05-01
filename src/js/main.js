cd29a923cd58badd43a9eb3eacae68fcd7071c2e
olopcodes <olopcodes@gmail.com>
2021-04-30 22:18:31 -0400

    added html template to the js file

---
+++ src/js/main.js
@@ -0,0 +1,46 @@
+// my-books html template
+// need to change the styling for read and unread
+
+/* <li class="my-books__item">
+<div class="my-books__heading">
+  <h3 class="my-books__title">another dope book tile</h3>
+  <a href="#" class="my-books__btn read">read</a>
+</div>
+<div class="my-books__description">
+  <p class="my-books__text">first lastname</p>
+  <p class="my-books__text">category</p>
+  <p class="my-books__text">1989</p>
+</div>
+</li> */
+
+// ntytimes html template
+// changes styles from show to none
+
+/* <li class="nytimes__item">
+<div class="nytimes__heading">
+  <span class="nytimes__rank"> 2 </span>
+  <img src="" alt="" class="nytimes__img" />
+  <h3 class="nytimes__title">great book title</h3>
+  <h3 class="nytimes__author">name author
+    <span class="nytimes__icon">
+      <i class="fas fa-chevron-right"></i>
+    </span>
+  </h3>
+  
+</div>
+<div class="nytimes__description">
+  <p class="nytimes__text">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui
+    distinctio nesciunt at. Corporis suscipit eos reiciendis
+    necessitatibus amet sunt error.
+  </p>
+  <div class="nytimes__links">
+    <a href="#" class="nytimes__link">link name</a>
+    <a href="#" class="nytimes__link">link name</a>
+    <a href="#" class="nytimes__link">link name</a>
+    <a href="#" class="nytimes__link">link name</a>
+    <a href="#" class="nytimes__link">link name</a>
+    <a href="#" class="nytimes__link">link name</a>
+  </div>
+</div>
+</li> */
