489fa893813c55a736c65a6c7bab919fbdb4742d
olopcodes <olopcodes@gmail.com>
2021-05-02 15:43:59 -0400

    added some more styles and started working on js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,71 @@
+import apiKey from "../../not-tracked/apikey.js";
+const form = document.querySelector(".form");
+const formInput = document.querySelector(".form__input");
+const booksNumberText = document.querySelector(".my-books__number");
+const bestSellerItems = document.querySelectorAll(".nytimes__item");
+
+class App {
+  constructor() {
+    form.addEventListener("submit", this._addBook.bind(this));
+  }
+
+  _addBook(e) {
+    e.preventDefault();
+    const bookTitle = formInput.value;
+    if (!bookTitle) return;
+    this._fetchBook(bookTitle);
+  }
+
+  async _fetchBook(title) {
+    const res = await fetch(
+      `http://openlibrary.org/search.json?title=${title}`
+    );
+    const data = await res.json();
+    if (data.numFound > 0) {
+      console.log(data);
+    } else {
+      //
+      alert(
+        `The book title: ${title.toUpperCase()} has not been found. Enter a new book title`
+      );
+    }
+  }
+}
+
+const app = new App();
+
+// include the html template in here
+const renderBookInfo = (book) => {
+  console.log(book.rank);
+  console.log(book.book_image);
+  console.log(book.title);
+  console.log(book.author);
+  console.log(book.description);
+  console.log(book.buy_links);
+};
+const getEbookBestSellers = async () => {
+  const res = await fetch(
+    `https://api.nytimes.com/svc/books/v3/lists/current/e-book-fiction.json?api-key=${apiKey}`
+  );
+
+  const data = await res.json();
+  console.log(data.results.books[0]);
+  data.results.books.forEach((book) => renderBookInfo(book));
+};
+
+// getEbookBestSellers();
+
+// class Book {
+// constructor(title, author)
+// }
+
+// for every added book, add an event listener to the item
+// listen for click on btn
+
+// add an event listener on every bestseller
+// and show description on click
+// class App {}
+
 // my-books html template
 // need to change the styling for read and unread
 
