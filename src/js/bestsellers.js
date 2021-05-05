ac0d60793ea80ea99c897665d17d60f966adc02f
olopcodes <olopcodes@gmail.com>
2021-05-04 22:28:28 -0400

    implemented bestsellers function and rendered books

--- src/js/bestsellers.js
+++ src/js/bestsellers.js
@@ -1,22 +1,48 @@
 import apiKey from "../../not-tracked/apikey";
-// (async () => {
-//   await this.handleMessage(msg);
-//   this.pendingMsgs--;
-//   if (cancelled && this.pendingMsgs === 0) {
-//     await chan.close();
-//     await this.amqpConnectionPool.release(conn);
-//   }
-// })();
+const nyTimesList = document.querySelector(".nytimes__list");
+
+(async () => {
+  const res = await fetch(
+    `https://api.nytimes.com/svc/books/v3/lists/current/e-book-fiction.json?api-key=${apiKey}`
+  );
+  const data = await res.json();
+  data.results.books.forEach((b) => renderBestseller(b));
+})();
 
 // include the html template in here
-const renderBookInfo = (book) => {
-  console.log(book.rank);
-  console.log(book.book_image);
-  console.log(book.title);
-  console.log(book.author);
-  console.log(book.description);
-  console.log(book.buy_links[0].name);
-  console.log(book.buy_links[0].url);
+const renderBestseller = (book) => {
+  nyTimesList.innerHTML += `
+  <li class="nytimes__item">
+  <div class="nytimes__heading">
+    <span class="nytimes__rank">${book.rank}</span>
+    <img src="${book.book_image}" alt="an image of ${book.title}" class="nytimes__img" />
+    <div class="ntyimes__details">
+    <h3 class="nytimes__title">${book.title}</h3>
+    <h3 class="nytimes__author">${book.author}
+      <span class="nytimes__icon">
+        <i class="fas fa-chevron-right"></i>
+      </span>
+    </h3>
+    </div>
+    
+    
+  </div>
+  <div class="nytimes__description">
+    <p class="nytimes__text">
+      ${book.description}
+    </p>
+    <div class="nytimes__links">
+      <a href="${book.buy_links[0].url}" class="nytimes__link">${book.buy_links[0].name}</a>
+      <a href="${book.buy_links[1].url}" class="nytimes__link">${book.buy_links[1].name}</a>
+      <a href="${book.buy_links[2].url}" class="nytimes__link">${book.buy_links[2].name}</a>
+      <a href="${book.buy_links[3].url}" class="nytimes__link">${book.buy_links[3].name}</a>
+      <a href="${book.buy_links[4].url}" class="nytimes__link">${book.buy_links[4].name}</a>
+      <a href="${book.buy_links[5].url}" class="nytimes__link">${book.buy_links[5].name}</a>
+    </div>
+  </div>
+  </li> 
+  
+  `;
 };
 const getEbookBestSellers = async () => {
   const res = await fetch(
@@ -28,36 +54,7 @@ const getEbookBestSellers = async () => {
   data.results.books.forEach((book) => renderBookInfo(book));
 };
 
-getEbookBestSellers();
+// getEbookBestSellers();
 
 // ntytimes html template
 // changes styles from show to none
-
-/* <li class="nytimes__item">
-<div class="nytimes__heading">
-  <span class="nytimes__rank"> 2 </span>
-  <img src="" alt="" class="nytimes__img" />
-  <h3 class="nytimes__title">great book title</h3>
-  <h3 class="nytimes__author">name author
-    <span class="nytimes__icon">
-      <i class="fas fa-chevron-right"></i>
-    </span>
-  </h3>
-  
-</div>
-<div class="nytimes__description">
-  <p class="nytimes__text">
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui
-    distinctio nesciunt at. Corporis suscipit eos reiciendis
-    necessitatibus amet sunt error.
-  </p>
-  <div class="nytimes__links">
-    <a href="#" class="nytimes__link">link name</a>
-    <a href="#" class="nytimes__link">link name</a>
-    <a href="#" class="nytimes__link">link name</a>
-    <a href="#" class="nytimes__link">link name</a>
-    <a href="#" class="nytimes__link">link name</a>
-    <a href="#" class="nytimes__link">link name</a>
-  </div>
-</div>
-</li> */
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,8 @@
+// import bestseller from "./bestsellers.js";
+
 const form = document.querySelector(".form");
 let myBooks = [];
 const myBooksList = document.querySelector(".my-books__list");
-const nyTimesList = document.querySelector(".nytimes__list");
 const formInput = document.querySelector(".form__input");
 const myBooksNumber = document.querySelector(".my-books__number");
 const bestSellerItems = document.querySelectorAll(".nytimes__item");
